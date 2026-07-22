484a31aff72848c2706bb61a7c1272059d9c62d9
Arthur <artwaintal@gmail.com>
2026-07-22 22:20:45 +0200

    Correct repo structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
-//! This library contains the game's logic.
-//! - `game_logic` : contains the rules and definitions for playing and biddings phases.
-//! - `server_logic` : contains the logic used by the server to update the state
+//! Everything used by both the server and the client
+
 
 #![allow(dead_code)]
-
-pub mod card_logic;
-pub mod server_logic;
+pub mod logic;
---
+++ src/logic.rs
@@ -0,0 +1,8 @@
+//! This library contains the game's logic.
+//! - `game_logic` : contains the rules and definitions for playing and biddings phases.
+//! - `server_logic` : contains the logic used by the server to update the state
+
+#![allow(dead_code)]
+
+pub mod card_logic;
+pub mod server_logic;
